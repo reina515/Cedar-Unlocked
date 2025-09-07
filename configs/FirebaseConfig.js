ea986d973b5330fa3642ff0ab32c6767d4edb922
HamedBouSaleh <hamedbousaleh@outlook.com>
2025-09-07 16:31:39 +0300

    Update FirebaseConfig.js

--- configs/FirebaseConfig.js
+++ configs/FirebaseConfig.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -17,9 +17,6 @@ const firebaseConfig = {
   measurementId: "G-GVNEHHGW0B"
 };
 
-// Initialize Firebase
-
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
