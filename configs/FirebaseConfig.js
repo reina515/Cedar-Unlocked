61fc01e18aa218a6e4a7663fe15794c00171ea8c
reina515 <rgh22@mail.aub.edu>
2025-09-07 17:00:57 +0300

    Update FirebaseConfig.js

--- configs/FirebaseConfig.js
+++ configs/FirebaseConfig.js
@@ -7,15 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCyoF_Gf09CUM4XDeqa-_eVGZM0Rw_XAoU",
-  authDomain: "ai-travel-planner-7b8cb.firebaseapp.com",
-  projectId: "ai-travel-planner-7b8cb",
-  storageBucket: "ai-travel-planner-7b8cb.firebasestorage.app",
-  messagingSenderId: "871698685561",
-  appId: "1:871698685561:web:2dd3395d27ddbd3f983ea8",
-  measurementId: "G-GVNEHHGW0B"
-};
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
